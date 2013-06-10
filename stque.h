457fddf9f9162d34d3c8a7ef6f3fabf195fea698
unknown <kokai_p@notebook_19.ALSTOMDOM.local>
2013-06-10 16:53:19 +0200

    complete stque

--- stque.h
+++ stque.h
@@ -1,7 +1,8 @@
 #ifndef _STACK_QUEUE_
 #define _STACK_QUEUE_
 
-#include <vector>
+#include <stack>
+#include <queue>
 
 template< bool Cond,
           typename True,
@@ -21,41 +22,96 @@ struct IF_<false,True,False>
 
 
 template< typename T,
-          bool Stack >
+          bool Stack = false,
+		  typename Container = std::deque< T > >
 class StackQueue
 {
 public:
-	struct false_;
-	struct true_;
+	typedef typename Container::size_type size_type;
+	typedef T 							  value_type;
 
-	void push( const T& value, false_ ) {
+	struct queue_ {};
+	struct stack_ {};
+	
+	template< typename Iterator >
+	StackQueue ( Iterator first, Iterator last )
+	{
+		for ( ; first != last; ++first )
+		{
+			push( *first );
+		}
+	}
+	
+	StackQueue( ) {}
+
+	void push( const T& value, queue_ ) {
 		_data.push_front( value );
 	}
 	
-	void push( const T& value, true_ ) {
-		_data.push_back( value );
+	void push( const T& value, stack_ ) {
+		_data.push_front( value );
 	}
 	
 	void push( const T& value )
 	{
-		push( value, IF_<Stack,true_,false_>::type );
+		push( value, typename IF_<Stack,stack_,queue_>::type() );
 	}
 
-	T get( const T& value, false_ ) const {
-		_data.front( value );
+	T get( queue_ ) const {
+		return _data.back( );
 	}
 	
-	T get( const T& value, true_ ) const {
-		_data.back( value );
+	T get( stack_ ) const {
+		return _data.front( );
 	}
 	
 	T get( ) const
 	{
-		get( IF_<Stack,true_,false_>::type );
+		return get( typename IF_<Stack,stack_,queue_>::type() );
+	}
+
+	T& get( queue_ ) {
+		return _data.back( );
+	}
+	
+	T& get( stack_ ) {
+		return _data.front( );
+	}
+	
+	T& get( )
+	{
+		return get( typename IF_<Stack,stack_,queue_>::type() );
+	}
+
+	void pop( queue_ ) {
+		_data.pop_back();
+	}
+	
+	void pop( stack_ ) {
+		_data.pop_front();
+	}
+	
+	void pop( )
+	{
+		pop( typename IF_<Stack,stack_,queue_>::type() );
+	}
+	
+	size_type size() const { return _data.size(); }
+	bool empty() const { return _data.empty(); }
+	
+	
+	std::stack< T, typename std::deque< T > > get_stack() const
+	{
+		return std::stack< T, typename std::deque< T > >( std::deque<T>( _data.begin(), _data.end() ) );
+	}
+	
+	std::queue< T > get_queue() const
+	{
+		return std::queue< T, typename std::deque< T > >( std::deque<T>( _data.begin(), _data.end() ) );
 	}
 
 private:
-	std::vector< T > _data;
+	Container _data;
 };
 
 
