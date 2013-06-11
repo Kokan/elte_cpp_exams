76d4914f736ac2826b6fc0812e1c5f85807e83ac
unknown <kokai_p@notebook_19.ALSTOMDOM.local>
2013-06-11 17:06:24 +0200

    continue autolist - mark 4

--- autolist.h
+++ autolist.h
@@ -4,10 +4,34 @@
 #include <list>
 #include <memory>
 
+
+
+
 template< typename T >
 class auto_list
 {
 public:
+	class auto_list_iterator : public std::list< T* >::iterator
+	{
+
+		
+	};
+	typedef auto_list_iterator iterator;
+
+	auto_list() {}
+	auto_list( auto_list< T > &al )
+	{
+		if ( this != &al )
+		{
+			_data.clear();
+			for ( iterator iter = al._data.begin();
+			      iter != al._data.end();
+				  ++iter )
+			{
+				push_front( new T( **iter ) );
+			}
+		}
+	}
 
 	~auto_list()
 	{
@@ -17,6 +41,14 @@ public:
 			_data.erase( _data.begin() );
 		}
 	}
+	
+	iterator begin() { return _data.begin(); }
+	iterator end()   { return _data.end();   }
+	
+	void insert( T* item ) { _data.insert( begin(), item ); }
+	
+	template< typename Iterator >
+	void insert( iterator position, Iterator first, Iterator last ) { _data.insert( position, first, last ); }
 	
 	void push_back( T* item )  { _data.push_back( item );  }
 	void push_front( T* item ) { _data.push_front( item ); }
